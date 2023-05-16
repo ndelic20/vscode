af3445e41b93656dde36465db002ce663568a12d
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-16 14:57:58 +0200

    4. labosi

---
+++ Evaluation Manager/Models/Evaluation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models {
+    public class Evaluation {
+
+        public Activity Activity { get; set; }
+        public Student Student { get; set; }    
+        public Teacher Evaluator  { get; set; }
+
+        public DateTime EvaluationDate { get; set; }
+
+        public int Points { get; set; }
+
+        public bool IsSufficientForGrade() {
+            return Points >= Activity.MinPointsForGrade;
+        }
+
+        public bool IsSufficinetForSignature() {
+            return Points >= Activity.MinPointsForSignature;
+        }
+
+
+
+    }
+}
